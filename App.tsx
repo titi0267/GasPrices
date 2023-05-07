36b2d22bea7580531cf4a2305fa9c495dafd57e2
Timothé Coniel <timothe@coniel.com>
2023-05-07 14:55:16 +0200

    Feat: init android build

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Main from './src';
+
+const App = (): JSX.Element => {
+  return <Main />;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import {View} from 'react-native';
+
+const Main = () => {
+  return <View style={{flex: 1, backgroundColor: 'red'}}></View>;
+};
+
+export default Main;
