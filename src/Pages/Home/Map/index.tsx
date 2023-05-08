0eb08498b5be6452b167aa168a9c2629ae05dadf
Timothé Coniel <timothe@coniel.com>
2023-05-08 21:57:50 +0200

    Feat: Gas stations refined around itinerary

--- src/Pages/Home/Map/index.tsx
+++ src/Pages/Home/Map/index.tsx
@@ -8,6 +8,7 @@ const Map = (props: {camera: any; start: any; end: any}) => {
   const [departmentCodes, setDepartmentCodes] = useState<string[]>([]);
   const [itinerary, setItinerary] = useState<any>(null);
   const [gasStations, setGasStations] = useState<any[]>([]);
+  const [refineGasStations, setRefineGasStations] = useState<any[]>([]);
 
   useEffect(() => {
     if (itinerary) {
@@ -29,8 +30,37 @@ const Map = (props: {camera: any; start: any; end: any}) => {
     }
   }, [departmentCodes]);
   useEffect(() => {
-    console.log(gasStations);
+    if (gasStations && itinerary) {
+      const myData: any[] = [];
+      const increment = Math.round(itinerary.geometry.coordinates.length / 100);
+      for (let i = 0; i < gasStations.length; i++) {
+        for (let j = 0; j < gasStations[i].nhits; j++) {
+          for (let d = 0; d < increment; d++) {
+            if (
+              itinerary.geometry.coordinates[d * increment][0] + 0.03 >=
+                gasStations[i].records[j].geometry.coordinates[0] &&
+              itinerary.geometry.coordinates[d * increment][0] - 0.03 <=
+                gasStations[i].records[j].geometry.coordinates[0]
+            ) {
+              if (
+                itinerary.geometry.coordinates[d * increment][1] + 0.03 >=
+                  gasStations[i].records[j].geometry.coordinates[1] &&
+                itinerary.geometry.coordinates[d * increment][1] - 0.03 <=
+                  gasStations[i].records[j].geometry.coordinates[1]
+              ) {
+                console.log(gasStations[i].records[j]);
+                myData.push(gasStations[i].records[j]);
+              }
+            }
+          }
+        }
+      }
+      setRefineGasStations(myData);
+    }
   }, [gasStations]);
+  useEffect(() => {
+    console.log(refineGasStations);
+  }, [refineGasStations]);
   return (
     <MapboxGl.MapView
       style={{height: '100%', width: '100%'}}
