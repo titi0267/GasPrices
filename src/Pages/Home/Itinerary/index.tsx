73c1ccbc2914bed868a5c1032da8a66161d6e4f7
Timothé Coniel <timothe@coniel.com>
2023-05-08 19:18:29 +0200

    Refacto: Map, Itinerary & department codes in separeted components

--- src/Pages/Home/Itinerary/index.tsx
+++ src/Pages/Home/Itinerary/index.tsx
@@ -2,89 +2,31 @@ import {useEffect, useState} from 'react';
 import fetchGeoJsonResults from '../../../services/geoJson.service';
 import MapboxGl from '@rnmapbox/maps';
 import fetchDepartmentCode from '../../../services/departmentCode.service';
+import goThroughItinerary from '../Map/DepartmentCodes';
 
-const goThroughItinerary = async (props: {
-  data: any;
-  setDepartmentCodes: (value: string[]) => void;
+const Itinerary = (props: {
+  start: any;
+  end: any;
+  itinerary: any;
+  setItinerary: (value: any) => void;
+  setDepartmentCodes: (value: any) => void;
 }) => {
-  const myData: string[] = [];
-  let interval = 100;
-  let dataToCheck = props.data.geometry.coordinates.length / 100;
-  if (dataToCheck > 10) {
-    dataToCheck = dataToCheck / 2;
-    interval /= 2;
-    console.log(interval);
-  }
-  console.log(dataToCheck);
-
-  if (dataToCheck <= 1) {
-    const resStart = await fetchDepartmentCode({
-      coords: props.data.geometry.coordinates[0],
-    });
-    const resEnd = await fetchDepartmentCode({
-      coords:
-        props.data.geometry.coordinates[
-          props.data.geometry.coordinates.length - 1
-        ],
-    });
-
-    const dataResultStart = await resStart.json();
-    const dataResultEnd = await resEnd.json();
-    if (!myData.includes(dataResultStart)) {
-      myData.push(dataResultStart);
-    }
-    if (!myData.includes(dataResultEnd)) {
-      myData.push(dataResultEnd);
-    }
-  } else {
-    for (let i = 0; i < dataToCheck; i++) {
-      const res = await fetchDepartmentCode({
-        coords: props.data.geometry.coordinates[i * interval],
-      });
-      if (!res.ok) {
-        continue;
-      }
-      try {
-        const dataResult = await res.json();
-        if (!myData.includes(dataResult)) {
-          myData.push(dataResult);
-        }
-      } catch (e) {}
-    }
-  }
-  props.setDepartmentCodes(myData);
-};
-
-const Itinerary = (props: {start: any; end: any}) => {
-  const [data, setData] = useState<any>(null);
-  const [departmentCodes, setDepartmentCodes] = useState<string[]>([]);
-
   useEffect(() => {
     if (props.start && props.end && props.start != null && props.end != null) {
-      setDepartmentCodes([]);
+      props.setDepartmentCodes([]);
       fetchGeoJsonResults(
         {
           start: props.start[0].toString() + ',' + props.start[1].toString(),
           end: props.end[0].toString() + ',' + props.end[1].toString(),
         },
-        setData,
+        props.setItinerary,
       );
     }
   }, [props.start, props.end]);
 
-  useEffect(() => {
-    if (data) {
-      goThroughItinerary({data: data, setDepartmentCodes: setDepartmentCodes});
-    }
-  }, [data]);
-
-  useEffect(() => {
-    console.log(departmentCodes);
-  }, [departmentCodes]);
-
-  if (data) {
+  if (props.itinerary) {
     return (
-      <MapboxGl.ShapeSource id="source" shape={data}>
+      <MapboxGl.ShapeSource id="source" shape={props.itinerary}>
         <MapboxGl.LineLayer
           id="line"
           style={{lineColor: 'blue', lineWidth: 3}}
--- src/Pages/Home/Map/index.tsx
+++ src/Pages/Home/Map/index.tsx
@@ -1,8 +1,23 @@
 import MapboxGl, {Camera} from '@rnmapbox/maps';
 import Itinerary from '../Itinerary';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import goThroughItinerary from './DepartmentCodes';
 
 const Map = (props: {camera: any; start: any; end: any}) => {
+  const [departmentCodes, setDepartmentCodes] = useState<string[]>([]);
+  const [itinerary, setItinerary] = useState<any>(null);
+
+  useEffect(() => {
+    if (itinerary) {
+      goThroughItinerary({
+        data: itinerary,
+        setDepartmentCodes: setDepartmentCodes,
+      });
+    }
+  }, [itinerary]);
+  useEffect(() => {
+    console.log(departmentCodes);
+  }, [departmentCodes]);
   return (
     <MapboxGl.MapView
       style={{height: '100%', width: '100%'}}
@@ -13,7 +28,13 @@ const Map = (props: {camera: any; start: any; end: any}) => {
         animationDuration={0}
         zoomLevel={3.5}
       />
-      <Itinerary start={props.start} end={props.end} />
+      <Itinerary
+        start={props.start}
+        end={props.end}
+        itinerary={itinerary}
+        setItinerary={setItinerary}
+        setDepartmentCodes={setDepartmentCodes}
+      />
     </MapboxGl.MapView>
   );
 };
