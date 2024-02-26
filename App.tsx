b00c71c69e629d08113a52efb9e1e846eb9b2339
Timothé Coniel <timothe@coniel.com>
2024-02-26 15:38:43 +0800

    Develop (#38)
    * Fix: env variables in github
    
    * Fix: Env variables in release
    
    * Feat: Update page check (#37)

--- App.tsx
+++ App.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigation from './src/Navigation/RootStackNavigator';
+import updateApp from './src/services/updateApp.service';
+import {Button, Linking, StyleSheet, Text, View} from 'react-native';
 
 const App = (): JSX.Element => {
+  const [version, setVersion] = useState(false);
+  useEffect(() => {
+    updateApp(setVersion);
+  }, []);
   return (
-    <NavigationContainer>
-      <RootStackNavigation />
-    </NavigationContainer>
+    <>
+      {version ? (
+        <View style={styles.updateContainer}>
+          <Text style={styles.text}>
+            Il semblerait que votre application ne soit pas à jour !
+          </Text>
+          <Button
+            title="Mettre à jour"
+            onPress={() => {
+              Linking.openURL('market://details?id=com.opticarbu');
+            }}
+          />
+        </View>
+      ) : (
+        <NavigationContainer>
+          <RootStackNavigation />
+        </NavigationContainer>
+      )}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  updateContainer: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    width: '80%',
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 18,
+  },
+});
 
 export default App;
