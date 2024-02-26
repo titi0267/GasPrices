ee68c19402d475296832aaa50de7bc3d50c72215
titi0267 <timothe@coniel.com>
2024-02-26 15:26:24 +0800

    Feat: Update page check

---
+++ src/services/updateApp.service.ts
@@ -0,0 +1,18 @@
+import {HOST} from '@env';
+
+const updateApp = async (setVersion: (value: any) => void) => {
+  const pj = require('../../package.json');
+  const body = {version: pj.version};
+  const res = await fetch(`${HOST}/update`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  if (!res.ok) throw Error('Error on osrm services');
+  const resolve = await res.json();
+  setVersion(resolve);
+};
+
+export default updateApp;
